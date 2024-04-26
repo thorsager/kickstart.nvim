4ee7eb8b6bb41e7d9dc9648f1b33682de81381ce
Michael Thorsager <thorsager@gmail.com>
2024-04-26 09:01:43 +0200

    Adding basic stuff

--- init.lua
+++ init.lua
@@ -200,6 +200,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- My stange and custom keypindings
+vim.keymap.set('n', '<leader>rw', '<cmd>:Ex<CR>', { desc = 'Open Net[RW]' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
