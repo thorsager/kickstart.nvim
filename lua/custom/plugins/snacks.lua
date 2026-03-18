19ce6282ac88d65127fb9c7af59321999d866a80
Michael Thorsager <thorsager@gmail.com>
2026-03-18 10:34:28 +0100

    feat(plugins): add snacks.nvim with custom config
    Added the folke/snacks.nvim plugin to enhance the Neovim UI experience.
    Configured various modules such as bigfile, indent, input, picker, notifier,
    quickfile, scope, scroll, statuscolumn, and words. Disabled dashboard and
    explorer modules for a more streamlined setup.

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,29 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+}
