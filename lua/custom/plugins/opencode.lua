a8ff2a9f3b587fe1a838b5fd809effa685ec3253
Michael Thorsager <thorsager@gmail.com>
2026-06-20 22:09:05 +0200

    feat(plugins): enable opencode.nvim integration
    Replace the commented-out disabled plugin block with an active
    configuration using vim.pack.add. Set up vim.g.opencode_opts
    for plugin options and enable autoread for events.reload.
    
    Add keymaps:
    - <leader>oa: ask OpenCode
    - <leader>os: select OpenCode action
    - go/goo: operator motions to append range/line
    - <S-C-u>/<S-C-d>: scroll OpenCode terminal

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -1,32 +1,23 @@
--- nickjvandyke/opencode.nvim (disabled)
--- vim.pack.add { 'https://github.com/nickjvandyke/opencode.nvim' }
---
--- require('opencode').setup {
---   -- snacks.nvim integration: see snacks.lua for picker/terminal/input config
--- }
---
--- vim.o.autoread = true -- Required for events.reload
---
--- vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
---   require('opencode').ask('@this: ', { submit = true })
--- end, { desc = 'Ask opencode…' })
--- vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
---   require('opencode').select()
--- end, { desc = 'Execute opencode action…' })
--- vim.keymap.set({ 'n', 't' }, '<C-.>', function()
---   require('opencode').toggle()
--- end, { desc = 'Toggle opencode' })
--- vim.keymap.set({ 'n', 'x' }, 'go', function()
---   return require('opencode').operator '@this '
--- end, { desc = 'Add range to opencode', expr = true })
--- vim.keymap.set('n', 'goo', function()
---   return require('opencode').operator '@this ' .. '_'
--- end, { desc = 'Add line to opencode', expr = true })
--- vim.keymap.set('n', '<S-C-u>', function()
---   require('opencode').command 'session.half.page.up'
--- end, { desc = 'Scroll opencode up' })
--- vim.keymap.set('n', '<S-C-d>', function()
---   require('opencode').command 'session.half.page.down'
--- end, { desc = 'Scroll opencode down' })
--- vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment under cursor', noremap = true })
--- vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement under cursor', noremap = true })
+vim.pack.add({
+  {
+    src = "https://github.com/nickjvandyke/opencode.nvim",
+    version = vim.version.range("*"), -- Latest stable release
+  },
+})
+
+---@type opencode.Opts
+vim.g.opencode_opts = {
+  -- Your configuration, if any; goto definition on the type for details
+}
+
+vim.o.autoread = true -- Required for `vim.g.opencode_opts.events.reload`
+
+-- Recommended/example keymaps
+vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end, { desc = "Ask OpenCode…" })
+vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,       { desc = "Select OpenCode…" })
+
+vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Append range to OpenCode", expr = true })
+vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Append line to OpenCode", expr = true })
+
+vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll OpenCode up" })
+vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll OpenCode down" })
